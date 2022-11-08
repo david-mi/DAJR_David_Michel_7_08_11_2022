b75bc5df918af50889a58e1ca1bda09a7099cd79
David Michel <david.michel2809@gmail.com>
2022-11-08 18:07:01 +0100

    feat: fetching, creating and displaying recipes to the dom on page load #7

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
 import "./styles/index.scss";
-console.log("init");
+import { getApiData } from "./api.js";
+import { createRecipeCard } from "./create";
+import { displayRecipeToDom } from "./display";
+import { recipesContainer } from "./constants";
+
+/**
+ * - Reset {@link recipesContainer} html content
+ * - Create each recipe html string and insert it to {@link recipesContainer}
+ * @param {Recipe[]} recipes 
+ */
+
+const createAndDisplayRecipes = (recipes) => {
+  recipesContainer.innerHTML = "";
+
+  recipes.forEach(recipe => {
+    const card = createRecipeCard(recipe);
+    displayRecipeToDom(card);
+    console.log(card);
+  });
+};
+
+(async () => {
+  const recipes = await getApiData();
+  createAndDisplayRecipes(recipes);
+})();
