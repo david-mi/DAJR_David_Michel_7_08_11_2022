fa5fedd65dd5e689bf91c7aa3d60675d2f0040f7
David Michel <david.michel2809@gmail.com>
2022-11-13 13:39:35 +0100

    fix: fixing eslint warnings

--- src/filter.js
+++ src/filter.js
@@ -7,31 +7,31 @@ import "./data/types.js";
  */
 
 
-const filterforLoop = (userInput, recipes) => {
-  const filteredRecipes = [];
-  const recipeLength = recipes.length;
+// const filterforLoop = (userInput, recipes) => {
+//   const filteredRecipes = [];
+//   const recipeLength = recipes.length;
 
-  for (let i = 0; i < recipeLength; i++) {
-    if (
-      recipes[i].name.toLowerCase().indexOf(userInput) !== -1 ||
-      recipes[i].description.toLowerCase().indexOf(userInput) !== -1
-    ) {
-      filteredRecipes.push(recipes[i]);
-      continue;
-    }
+//   for (let i = 0; i < recipeLength; i++) {
+//     if (
+//       recipes[i].name.toLowerCase().indexOf(userInput) !== -1 ||
+//       recipes[i].description.toLowerCase().indexOf(userInput) !== -1
+//     ) {
+//       filteredRecipes.push(recipes[i]);
+//       continue;
+//     }
 
-    const ingredientsLength = recipes[i].ingredients.length;
+//     const ingredientsLength = recipes[i].ingredients.length;
 
-    for (let j = 0; j < ingredientsLength; j++) {
-      if (recipes[i].ingredients[j].ingredient.toLowerCase().indexOf(userInput) !== -1) {
-        filteredRecipes.push(recipes[i]);
-        break;
-      }
-    }
-  }
+//     for (let j = 0; j < ingredientsLength; j++) {
+//       if (recipes[i].ingredients[j].ingredient.toLowerCase().indexOf(userInput) !== -1) {
+//         filteredRecipes.push(recipes[i]);
+//         break;
+//       }
+//     }
+//   }
 
-  return filteredRecipes;
-};
+//   return filteredRecipes;
+// };
 
 export const filterRecipes = (userInput, recipes) => {
   return recipes.filter((recipe) => {
