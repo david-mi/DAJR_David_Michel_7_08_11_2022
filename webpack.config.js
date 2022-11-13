98163b44907bc7c7f039dfc5f28aa7144f01a6d7
David Michel <david.michel2809@gmail.com>
2022-11-13 13:54:56 +0100

    feat: webpack config modification

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, "src/app.js"),
   output: {
-    publicPath: "/",
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js"
   },
