682711c7b6c7ae1b3b28881f994c50870af179c4
David Michel <david.michel2809@gmail.com>
2022-11-16 18:58:00 +0100

    feat: adding comments to resizeObserver and removing flex order management

--- src/resizeObserver.js
+++ src/resizeObserver.js
@@ -1,19 +1,20 @@
 const optionsElements = document.querySelectorAll(".option");
 
+/**
+ * Solution found to keep options menu as responsive as possible with displayed option list overlapping recipes container
+ * - Observe every dom resizes happening to options elements Element
+ * - Adapt option negative margin bottom to keep option list overlap
+ */
+
 const resizeObserver = new ResizeObserver((entries) => {
+
   entries.forEach(({ target }) => {
 
     if (target.dataset.display !== undefined) {
       const targetedListElement = target.querySelector(".list");
       const listHeight = targetedListElement.getBoundingClientRect().height;
       target.style.marginBottom = `-${listHeight}px`;
-
-      if (innerWidth < 1150) {
-        target.style.order = 1;
-      }
-
     } else {
-      target.style.order = "unset";
       target.style.marginBottom = "0";
     }
   });
