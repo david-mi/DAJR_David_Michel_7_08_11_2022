4981b4738634a1e08494ea962321359a9a9a0f3a
David Michel <david.michel2809@gmail.com>
2022-11-17 08:56:14 +0100

    style: handling option list overflow with only css, removing resizeObserver

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import "./styles/index.scss";
-import "./resizeObserver.js";
 import "./handlers.js";
 import { recipesData } from "./data/recipesData";
 import { createAndDisplayOptionsLists, createAndDisplayRecipes } from "./view.js";
-
-
 
 createAndDisplayRecipes(recipesData.recipes);
 createAndDisplayOptionsLists(recipesData.recipes);
