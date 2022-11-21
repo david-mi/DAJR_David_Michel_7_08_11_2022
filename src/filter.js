c73c8efd04816f0b2febb072d163d1e82be8ce52
David Michel <david.michel2809@gmail.com>
2022-11-21 16:35:33 +0100

    feat: adding other filterRecipes methods

--- src/filter.js
+++ src/filter.js
@@ -14,17 +14,79 @@ import { formatString } from "./utils.js";
  * @return {Recipe[]} filtered recipe based on recipe's ingredients, name and description
  */
 
-
 export const filterRecipes = (userInput, recipes) => {
-  return recipes.filter(({ name, description, ingredients }) => {
-    return (
-      formatString(name).indexOf(userInput) !== -1 ||
-      formatString(description).indexOf(userInput) !== -1 ||
-      ingredients.some(({ ingredient }) => {
-        return formatString(ingredient).indexOf(userInput) !== -1;
-      })
-    );
-  });
+  const filteredRecipes = [];
+
+  for (let i = 0; i < recipes.length; i++) {
+    if (
+      formatString(recipes[i].name).indexOf(userInput) !== -1 ||
+      formatString(recipes[i].description).indexOf(userInput) !== -1
+    ) {
+      filteredRecipes.push(recipes[i]);
+      continue;
+    }
+
+    for (let j = 0; j < recipes[i].ingredients.length; j++) {
+      if (formatString(recipes[i].ingredients[j].ingredient).indexOf(userInput) !== -1) {
+        filteredRecipes.push(recipes[i]);
+        break;
+      }
+    }
+  }
+
+  return filteredRecipes;
+};
+
+
+/**
+ * @param {string} userInput text input from search input in lowercase
+ * @param {string} recipes recipes to filter
+ * @return {Recipe[]} filtered recipe based on recipe's ingredients, name and description
+ */
+
+export const filterRecipesNoIndexOf = (userInput, recipes) => {
+  const filteredRecipes = [];
+
+  function isStrIncluded(strToCompare, str) {
+    if (str.length === 0) return true;
+
+    for (let i = 0; i < strToCompare.length; i++) {
+      let matchCount = 0;
+
+      for (let k = 0; k < str.length; k++) {
+        if (strToCompare[i + k] === str[k]) {
+          matchCount++;
+        } else {
+          matchCount = 0;
+        }
+
+        if (matchCount === str.length) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  for (let i = 0; i < recipes.length; i++) {
+    if (
+      isStrIncluded(formatString(recipes[i].name), userInput) ||
+      isStrIncluded(formatString(recipes[i].description), userInput)
+    ) {
+      filteredRecipes.push(recipes[i]);
+      continue;
+    }
+
+    for (let j = 0; j < recipes[i].ingredients.length; j++) {
+      if (isStrIncluded(formatString(recipes[i].ingredients[j].ingredient), userInput)) {
+        filteredRecipes.push(recipes[i]);
+        break;
+      }
+    }
+  }
+
+  return filteredRecipes;
 };
 
 
